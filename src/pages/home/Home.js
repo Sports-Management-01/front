440e2730b5ace5a30defc5b3bf7c76d90487a835
Meis01 <meis.baas95@hotmail.com>
2023-02-03 16:22:20 +0300

    working on DisplayFields

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import Header from "../../components/Header/Header"
+import Hero from "../../components/Hero/Hero"
+import HomeField from "../../components/HomeFields/HomeFields"
 
 
 const Home = () => {
     return (
         <>
             <Header />
+            <Hero/>
+            <HomeField/>
         </>
     )
 }
