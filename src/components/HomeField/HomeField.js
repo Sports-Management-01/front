a26da473a85e729a42ed6f95eecb7624527ca44e
Feras Jobeir <fjobeir@gmail.com>
2023-02-03 16:56:59 +0300

    Field

--- src/components/HomeField/HomeField.js
+++ src/components/HomeField/HomeField.js
@@ -1,16 +1,6 @@
 import img1 from "../../assets/img/home-room/img1.jpg";
-import { useContext, useEffect, useRef, useState } from "react";
 
-const HomeField = async ({ data }) => {
-  const response = await fetch(`http://localhost:3000/fields/${data.id}}`, {
-    method: "GET",
-  });
-  const json = await response.json();
-  console.log(json.data);
-  if (json.success) {
-    console.log(json.data);
-  }
-
+const HomeField = ({data}) => {
   return (
     <>
       <div className="col-lg-3 col-md-6 col-sm-6 p-0">
@@ -24,7 +14,7 @@ const HomeField = async ({ data }) => {
           <div class="home__room__title">
             <h4>{data.name}</h4>
             <h2>
-              <sup>$</sup>55<span>/day</span>
+              <sup>$</sup>{data.hourPrice}<span>/hour</span>
             </h2>
           </div>
           <a href="#">Booking Now</a>
--- src/components/HomeFields/HomeFields.js
+++ src/components/HomeFields/HomeFields.js
@@ -1,72 +1,66 @@
- import { AuthContext } from "../../contexts/AuthContext";
-import { useContext, useEffect,useRef,useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext, useEffect, useRef, useState } from "react";
 import HomeField from "../HomeField/HomeField";
 
-const HomeFields = () =>{
-const {fields, setFields}  = useContext(AuthContext);
-
-const allFields = async () => {
- 
-const res = await fetch(`http://localhost:3000/fields`,{
-    method: 'GET',
-  
-});
-const json = await res.json();
-console.log(json)
-if(json.success){
-    console.log(`fielddddd`)
-    setFields(fields)
-    console.log(setFields)
-}
-else{window.alert("There is no Field!")
-console.log(json.data)}
-}
-useEffect(() => {
-    allFields();
-},[]);
-
-return(
-    <>
+const HomeFields = () => {
+    const [fields, setFields] = useState([])
+    const allFields = async () => {
+        const res = await fetch(`http://localhost:3000/fields`, {
+            method: 'GET',
+        });
+        const json = await res.json();
+        if (json.success) {
+            console.log(`fielddddd`)
+            setFields(json.data)
+        }
+        else {
+            window.alert("There is no Field!")
+            console.log(json.data)
+        }
+    }
     
-    <section className="home-room spad">
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="section-title">
-                        <h5>OUR Courts</h5>
-                        <h2>Explore Our Courts</h2>
+    useEffect(() => {
+        allFields();
+    }, []);
+
+    return (
+        <>
+
+            <section className="home-room spad">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="section-title">
+                                <h5>OUR Courts</h5>
+                                <h2>Explore Our Courts</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className="container-fluid">
-            <div className="row">
-                
-                {
-    fields?.map((field,i)=>(<HomeField key={i} data={field}/>))
-    
-                 }
-                   
-              
-            </div>
-        </div>
-        <div className="container">
-            <div className="home__explore">
-                <div className="row">
-                    <div className="col-lg-9 col-md-8">
-                        <h3>Planning your next trip? Save up to 25% on your hotel</h3>
-                    </div>
-                    <div className="col-lg-3 col-md-4 text-center">
-                        <a href="#" className="primary-btn">Explorer More</a>
+                <div className="container-fluid">
+                    <div className="row">
+                        {
+                            fields.slice(0, 4)?.map((field, i) => (<HomeField key={i} data={field} />))
+                        }
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-   
-    </>
-)
-    
+                <div className="container">
+                    <div className="home__explore">
+                        <div className="row">
+                            <div className="col-lg-9 col-md-8">
+                                <h3>Planning your next trip? Save up to 25% on your hotel</h3>
+                            </div>
+                            <div className="col-lg-3 col-md-4 text-center">
+                                <a href="#" className="primary-btn">Explorer More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+
 }
 
 export default HomeFields; 
