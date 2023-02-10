35251a41bf1135a4bf828736e1de7b0d04328c09
Meis01 <meis.baas95@hotmail.com>
2023-02-10 18:01:23 +0300

    companyDashboard was updated

--- src/pages/companyDashboard/Fields.js
+++ src/pages/companyDashboard/Fields.js
@@ -1,14 +1,106 @@
 
-import React from 'react';
-import SideNav from '../../components/SideNav/SideNav';
-import Nav from '../../components/Nav/Nav';
+
+import SideNav from "../../components/SideNav/SideNav";
+import Nav from "../../components/Nav/Nav";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext, useEffect,useState  } from "react";
+const dayjs = require('dayjs')
 
 const Fields = () => {
+    const {token} = useContext(AuthContext);
+    const { id } = useParams();
+    const { user, setUser } = useContext(AuthContext);
+    const [fieldDetails, setFieldDetails] = useState([]);
+    const getMyFields = async () => {
+        const res = await fetch(`http://localhost:3000/fields/company/fields`, {
+          method: "GET",
+          body: null,
+          headers: {
+            "content-type" : "application/json",
+            "Authorization": `Bearer ${token}`,
+          },
+        });
+        console.log(user.id)
+        const json = await res.json();
+        console.log(json)
+        if(json.success){
+         setFieldDetails(json.data);
+          console.log(json.data);
+        }
+        
+      };
+      useEffect(() => {
+        getMyFields();
+      }, []);
+    
+      console.log(fieldDetails)
+
+
+
+
     return (
       <>
         <Nav/>
       <div className='user-hero' style={{display:"flex"}}>
       <SideNav/>
+      <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="profile-data">
+                <div className="table">
+                  <div className="col-12 p-3 mb-4 bottom-border">
+                    {/* blue area info */}
+                    <div className="alert alert-info">My Courts</div>
+                    <table className="table">
+                      <tr>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Price/<small>h</small></th>
+                        <th>Working Hours</th>
+                        <th>State</th>
+                        <th>Status</th>
+                        <th>options</th>
+                     
+
+
+                      </tr>
+                     
+                          <tr>
+                      
+                          <td>name</td>
+                          
+                          <td>name</td>
+                          <td> name</td>
+                   
+                          
+                            
+                          
+                         
+                          <td>{/* {dayjs(reservation.from).format('ddd,MMM D, YYYY h:mm A')} */}</td>
+                          <td>{/* {dayjs(reservation.to).format('ddd,MMM D, YYYY h:mm A')} */}</td>
+                          <td>price</td>
+                          
+                          <td>
+                            <span className="badge badge-danger"> canceled</span>
+                         <span className="badge badge-warning">Passed</span> <><span class="badge badge-primary">Acitve</span></>  
+                          
+                          </td>
+                          <td><>
+                          <input className="btn-danger btn" type="button" value="Cancel"  />
+                          </></td>
+
+
+                        </tr>
+
+                     
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       </>
