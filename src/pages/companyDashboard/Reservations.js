48fc85bede6553c2eb095f5143c3e486568d5126
shafeeqhawa <shafeeq1996@hotmail.com>
2023-02-13 10:25:42 +0300

    edit reservations

--- src/pages/companyDashboard/Reservations.js
+++ src/pages/companyDashboard/Reservations.js
@@ -1,49 +1,47 @@
-
-
-import React from 'react';
-import Nav from '../../components/Nav/Nav';
-import SideNav from '../../components/SideNav/SideNav';
+import React from "react";
+import Nav from "../../components/Nav/Nav";
+import SideNav from "../../components/SideNav/SideNav";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useContext, useEffect, useState } from "react";
-const dayjs = require('dayjs')
-
+const dayjs = require("dayjs");
 
 const Reservations = () => {
-  const {token} = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
   const { id } = useParams();
-const { user, setUser } = useContext(AuthContext);
-const [reservationDetails, setReservationgDetails] = useState([]);
+  const { user, setUser } = useContext(AuthContext);
+  const [fieldDetails, setReservationgDetails] = useState([]);
   const getResevrations = async () => {
-    const res = await fetch(`http://localhost:3000/reservations/companyreservation`, {
-      method: "GET",
-      body: null,
-      headers: {
-        "content-type" : "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log(user.id)
+    const res = await fetch(
+      `http://localhost:3000/reservations/companyreservation`,
+      {
+        method: "GET",
+        body: null,
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(user.id);
     const json = await res.json();
-    console.log(json)
-    
+    console.log(json);
 
-    if(json.success){
+    if (json.success) {
       setReservationgDetails(json.data);
       console.log(json.data);
     }
-    console.log(setReservationgDetails)
-    
+    console.log(setReservationgDetails);
   };
   useEffect(() => {
     getResevrations();
   }, []);
-    return (
-        <>
-        <Nav/>
-      <div className='user-hero' style={{display:"flex"}}>
-      <SideNav/>
-      <div className="container-fluid">
+  return (
+    <>
+      <Nav />
+      <div className="user-hero" style={{ display: "flex" }}>
+        <SideNav />
+        <div className="container-fluid">
           <div className="row">
             <div className="col-12">
               <div className="profile-data">
@@ -62,83 +60,40 @@ const [reservationDetails, setReservationgDetails] = useState([]);
                         <th>Cost</th>
                         <th>Options</th>
                       </tr>
-                      
-                      {
-                        reservationDetails.map((reservation, i)=>(
-                          <tr>
-                          <td>
-                          {
-                              reservation.Reservations.map((re,i)=>(
-                                <>
-                                {re.User.name}
-                                </>
-                              ))
-                            }
 
+                      {fieldDetails.map((field, i) => (
+                        <>
+                          {field.Reservations.map((re, i) => (
+                            <tr>
+                              <td>{re.User.name}</td>
+                              <td>{field.Category.name}</td>
+                              <td>{field.name}</td>
+                              <td>
+                                {re.ReservationEquipments.map((equ, i) => (
+                                  <>
+                                    {equ.Equipment.name}
+                                    {equ.Equipment.multiple && (
+                                      <>({equ.count}) </>
+                                    )}
+                                    {i <
+                                      re.ReservationEquipments.length - 1 && (
+                                      <>,</>
+                                    )}
+                                  </>
+                                ))}
+                              </td>
+                              <td>
+                                {dayjs(re.from).format(
+                                  "ddd,MMM D, YYYY h:mm A"
+                                )}
+                              </td>
 
-                          </td>
-                          
-                          <td>{reservation.Category.name}</td>
-                          <td>{reservation.name}</td>
-                          <td>
-                            {
-                           reservation.Reservations.map((eq, i)=>(
-                           <>
-                           {
-                            eq.ReservationEquipments.map((equ,i)=>(
-                              <>
-                             {equ.Equipment.name} 
-                             {equ.Equipment.multiple && 
-                              <> 
-                              ({equ.count}) </>}
-                              {i < eq.ReservationEquipments.length -1 && (
-                              <>
-                              ,
-                              </>
-                            )}
-                            
+                              <td>
+                                {dayjs(re.to).format("ddd,MMM D, YYYY h:mm A")}
+                              </td>
+                              <td>{re.total}$</td>
 
-                              </>
-
-                            ))
-                    
-                           }
-                           
-                           </>
-                          ))}
-                          </td>
-                          <td>
-                          {
-                              reservation.Reservations.map((re,i)=>(
-                                <>
-                                {dayjs(re.from).format('ddd,MMM D, YYYY h:mm A')}
-
-                                </>
-                              ))
-                            }
-                            </td>
-                          
-                            <td>
-                              {
-                              reservation.Reservations.map((re,i)=>(
-                                <>
-                               {dayjs(re.to).format('ddd,MMM D, YYYY h:mm A')}
-
-                                </>
-                              ))
-                            }
-                            </td>
-                            <td>{
-                              reservation.Reservations.map((re,i)=>(
-                                <>
-                               {re.total}$
-
-                                </>
-                              ))
-                            }</td>
-
-                         
-                          {/* <td>{dayjs(reservation.from).format('ddd,MMM D, YYYY h:mm A')}</td>
+                              {/* <td>{dayjs(reservation.from).format('ddd,MMM D, YYYY h:mm A')}</td>
                           <td>{dayjs(reservation.to).format('ddd,MMM D, YYYY h:mm A')}</td>
                           <td>{reservation.total}$</td>
                           
@@ -147,16 +102,17 @@ const [reservationDetails, setReservationgDetails] = useState([]);
                           : dayjs(reservation.to).isBefore(dayjs(new Date())) ? <><span className="badge badge-warning">Passed</span></> : <><span class="badge badge-primary">Acitve</span></>  
                           }
                           </td> */}
-                          <td><>
-                          <input className="btn-danger btn" type="button" value="Cancel"  />
-                          </></td>
-
-
-                        </tr>
-                        ))
-                      }
-
-                     
+                              <td>
+                                <input
+                                  className="btn-danger btn"
+                                  type="button"
+                                  value="Cancel"
+                                />
+                              </td>
+                            </tr>
+                          ))}
+                        </>
+                      ))}
                     </table>
                   </div>
                 </div>
@@ -165,10 +121,8 @@ const [reservationDetails, setReservationgDetails] = useState([]);
           </div>
         </div>
       </div>
-      
-
-      </>
-    );
-}
+    </>
+  );
+};
 
 export default Reservations;
