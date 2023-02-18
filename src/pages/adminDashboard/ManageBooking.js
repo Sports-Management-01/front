206ff36c73cc5fe4e74660b33aa21adf2109b4e8
shafeeqhawa <shafeeq1996@hotmail.com>
2023-02-18 11:35:35 +0300

    update managebooking

--- src/pages/adminDashboard/ManageBooking.js
+++ src/pages/adminDashboard/ManageBooking.js
@@ -4,6 +4,8 @@ import SideNav from "../../components/SideNav/SideNav";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useContext, useEffect, useState, useRef } from "react";
+import {Modal, Button} from 'react-bootstrap';
+
 const dayjs = require("dayjs");
 
 const ManageBooking = () => {
@@ -11,6 +13,11 @@ const ManageBooking = () => {
   const { id } = useParams();
   const { user, setUser } = useContext(AuthContext);
   const [reservationDetails, setReservationgDetails] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const cancelationReason = useRef();
+  const [counter, setCounter] = useState(0);
 
 
   
@@ -36,10 +43,32 @@ const ManageBooking = () => {
     console.log(setReservationgDetails);
   };
   
-
+  const cancelReservation = async(id) =>{
+    console.log(cancelationReason.current.value ) 
+     const res = await fetch(`http://localhost:3000/reservations/${id}`,
+     {
+       method: "DELETE",
+       body: JSON.stringify({ cancelationReason: cancelationReason.current.value }),
+ 
+       headers:{
+         "Content-Type":"application/json",
+          "Authorization": `Bearer ${token}`,
+       }
+     });
+     const json = await res.json();
+     if (json.success){
+         console.log(json)
+         window.alert(json.messages)
+        setCounter(counter+1) 
+        console.log(counter)
+     }
+     else{
+         window.alert(json.messages)
+     }
+   }
   useEffect(() => {
     getResevrations();
-  }, []);
+  }, [counter]);
   return (
     <>
      <Nav />
@@ -71,7 +100,8 @@ const ManageBooking = () => {
                           
                             <tr>
                               <td>{reservation?.User?.name}</td>
-                              <td></td>
+                             <td>
+                           </td>
                               <td>{reservation?.Field?.Category?.name}</td>
                               <td>{reservation?.Field?.name}</td>
                               <td>
@@ -104,6 +134,41 @@ const ManageBooking = () => {
                           : dayjs(reservation.to).isBefore(dayjs(new Date())) ? <><span className="badge badge-warning">Passed</span></> : <><span class="badge badge-primary">Acitve</span></>  
                           }
                           </td> 
+                          
+                          <td>
+                                <button
+                                  className="btn-danger btn"
+                                  type="button"
+                                  value="Cancel"
+                                  onClick={handleShow}
+                                >Cancel</button>
+                                {/*  <!-- Modal --> */}
+                                <Modal show={show} onHide={handleClose}>
+                               
+                                <Modal.Header closeButton >
+                                
+                                  <Modal.Title>Modal heading</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body >   <input style={{width:460, height:50,border:0}}
+                                                ref={cancelationReason}
+                                                type="text"
+                                                placeholder="What is the reason?"/>
+                                              </Modal.Body>
+                                <Modal.Footer>
+                                  <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                  </Button>
+                                  <Button variant="primary" onClick={()=>{
+
+                                      cancelReservation(reservation.id)
+                                      handleClose()
+                                  }}>
+                                    Save Changes
+                                  </Button>
+                                </Modal.Footer>
+                              </Modal>
+                                {/* END Model */}
+                              </td>
 
                           
                             </tr>
