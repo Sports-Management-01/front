67bff2f2a68613d82775461a17303c7fd342e565
shafeeqhawa <shafeeq1996@hotmail.com>
2023-02-23 23:42:27 +0300

    updated home page

--- src/components/HomeFields/HomeFields.js
+++ src/components/HomeFields/HomeFields.js
@@ -1,76 +1,111 @@
-import useEmblaCarousel from 'embla-carousel-react'
+import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Field from "../../pages/exploreFields/Field";
-import { shuffle } from '../../utils/utils';
-import './HomeFields.css';
-
+import { shuffle } from "../../utils/utils";
+import "./HomeFields.css";
+import logo from "../../logo.svg";
 const HomeFields = () => {
-    const navigate = useNavigate();
-    const [emblaRef] = useEmblaCarousel()
-    const [fields, setFields] = useState([])
-    const allFields = async () => {
-        const res = await fetch(`http://localhost:3000/fields`, {
-            method: 'GET',
-        });
-        const json = await res.json();
-        if (json.success) {
-            console.log(`fielddddd`)
-            setFields(json.data)
-            emblaRef.current.reInit()
-        }
-        else {
-            window.alert("There is no Field!")
-            console.log(json.data)
-        }
+  const navigate = useNavigate();
+  const [emblaRef] = useEmblaCarousel();
+  const [fields, setFields] = useState([]);
+  const [companies, setcompanies] = useState([]);
+
+  const allFields = async () => {
+    const res = await fetch(`http://localhost:3000/fields`, {
+      method: "GET",
+    });
+    const json = await res.json();
+    if (json.success) {
+      console.log(`fielddddd`);
+      setFields(json.data);
+      emblaRef.current.reInit();
+    } else {
+      window.alert("There is no Field!");
+      console.log(json.data);
     }
+  };
 
-    useEffect(() => {
-        allFields();
-    }, []);
+  const allCompanies = async () => {
+    const res = await fetch(`http://localhost:3000/users/companies`, {
+      method: "GET",
+    });
+    const json = await res.json();
+    if (json.success) {
+      console.log(`all companies`);
+      setcompanies(json.data);
+    } else {
+      window.alert("There is no Companies!");
+      console.log(json.data);
+    }
+  };
 
-    return (
-        <>
-            <section className="home-room spad">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="section-title">
-                                <h5>OUR Courts</h5>
-                                <h2>Explore Our Courts</h2>
-                            </div>
-                        </div>
-                    </div>
+  useEffect(() => {
+    allFields();
+    allCompanies();
+  }, []);
+
+  return (
+    <>
+      <section className="home-room spad">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="section-title">
+                <h5>OUR Courts</h5>
+                <h2>Explore Our Courts</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row ">
+            {shuffle(fields.splice(0, 6))?.map((field, i) => (
+              <div key={i} className="col col-sm-6 col-md-4">
+                <Field field={field} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="container">
+          <div className="home__explore">
+            <div className="row">
+              <div className="col-lg-9 col-md-8">
+                <h3>
+                  Planning your next match? Save up to 25% on your challange
+                </h3>
+              </div>
+              <div className="col-lg-3 col-md-4 text-center">
+                <Link className="primary-btn " to={"/courts"}>
+                  Explore More
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container flex-row">
+          <div className="d-flex col-12 align-items-center justify-content-center">
+            {companies.map((company, i) => {
+              return (
+                <div className="d-flex align-items-center m-3 ">
+                  <img
+                    src={company?.image ? company?.image : logo}
+                    style={{
+                      width: "50",
+                      height: "50px",
+                      borderRadius: "10px",
+                    }}
+                  />
+                  <div>{company?.name}</div>
                 </div>
-                <div className="container">
-                    <div className="row ">
-                                {
-                                    shuffle(fields.splice(0, 6))?.map((field, i) => (
-                                        <div key={i} className="col col-sm-6 col-md-4">
-                                            <Field field={field} />
-                                        </div>
-                                    ))
-                                }
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="home__explore">
-                        <div className="row">
-                            <div className="col-lg-9 col-md-8">
-                                <h3>Planning your next match? Save up to 25% on your challange</h3>
-                            </div>
-                            <div className="col-lg-3 col-md-4 text-center">
-                                <Link className="primary-btn " to={"/exploreFields"}>Explore More</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-        </>
-    )
-
-}
-
-export default HomeFields; 
+export default HomeFields;
--- src/pages/fieldDetails/FieldDetails.js
+++ src/pages/fieldDetails/FieldDetails.js
@@ -4,246 +4,222 @@ import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { Link } from "react-router-dom";
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-import './FieldDetails.css'
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import "./FieldDetails.css";
 
 const FieldDetails = () => {
-	const [emblaRef] = useEmblaCarousel({}, [Autoplay({ delay: 4000 })])
-	const { id } = useParams();
-	const [field, setField] = useState({
-		image: [],
-	});
-	const [equipments, setEquipments] = useState([]);
+  const [emblaRef] = useEmblaCarousel({}, [Autoplay({ delay: 4000 })]);
+  const { id } = useParams();
+  const [field, setField] = useState({
+    image: [],
+  });
+  const [equipments, setEquipments] = useState([]);
 
-	useEffect(() => {
-		fetch(`http://localhost:3000/fields/${id}`)
-			.then((response) => {
-				return response.json().then((json) => {
-					setField(json.data);
-				});
-			})
-			.catch();
-	}, [id]);
-	const getAllEquipments = async () => {
-		const res = await fetch(`http://localhost:3000/equipments`, {
-			method: "GET",
-		});
-		const json = await res.json();
-		if (json.success) {
-			console.log(json.success);
-			setEquipments(json.data);
-			console.log(json.data);
-		} else {
-			window.alert("There is no Field!");
-			console.log(json.data);
-		}
-	};
-	useEffect(() => {
-		getAllEquipments();
-	}, []);
+  useEffect(() => {
+    fetch(`http://localhost:3000/fields/${id}`)
+      .then((response) => {
+        return response.json().then((json) => {
+          setField(json.data);
+        });
+      })
+      .catch();
+  }, [id]);
+  const getAllEquipments = async () => {
+    const res = await fetch(`http://localhost:3000/equipments`, {
+      method: "GET",
+    });
+    const json = await res.json();
+    if (json.success) {
+      console.log(json.success);
+      setEquipments(json.data);
+      console.log(json.data);
+    } else {
+      window.alert("There is no Field!");
+      console.log(json.data);
+    }
+  };
+  useEffect(() => {
+    getAllEquipments();
+  }, []);
 
-	return (
-		<>
-			<Header />
-			<div className="row d-flex flex-column flex-md-row align-items-center">
-				<div className="col p-3 px-5">
-					<h2 className="mt-5">{field.name}</h2>
-					<div className="row mt-3 w-100 room__details__facilities">
-						<div className="col-lg-6">
-							<ul>
-								<li>
-									<span className="icon_check"></span> Takami Bridal
-									Attire
-								</li>
-								<li>
-									<span className="icon_check"></span> Esthetic
-									Salon
-								</li>
-								<li>
-									<span className="icon_check"></span> Multilingual
-									staff
-								</li>
-								<li>
-									<span className="icon_check"></span> Dry cleaning
-									and laundry
-								</li>
-								<li>
-									<span className="icon_check"></span> Credit cards
-									accepted
-								</li>
-							</ul>
-						</div>
-						<div className="col-lg-6">
-							<ul>
-								<li>
-									<span className="icon_check"></span> Rent-a-car
-								</li>
-								<li>
-									<span className="icon_check"></span> Reservation &
-									confirmation
-								</li>
-								<li>
-									<span className="icon_check"></span> Babysitter
-									upon request
-								</li>
-								<li>
-									<span className="icon_check"></span> 24-hour
-									currency exchange
-								</li>
-								<li>
-									<span className="icon_check"></span> 24-hour
-									Manager on Duty
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div className="col">
-					<div className="embla" ref={emblaRef}>
-						<div className="embla__container">
-							{field?.image.map((img, j) => {
-								return <div key={j} className='embla__slide embla__slide__padded' style={{
-									backgroundImage: `url(${img.url})`,
-									backgroundSize: 'cover'
-								}}></div>
-							})}
-						</div>
-					</div>
-				</div>
-			</div>
+  return (
+    <>
+      <Header />
+      <div className="row d-flex flex-column flex-md-row align-items-center">
+        <div className="col p-3 px-5">
+          <h2 className="mt-5">{field.name}</h2>
+          <div className="row mt-3 w-100 room__details__facilities">
+            <div className="col-lg-6">
+              <ul>
+                <li>
+                  <span className="icon_check"></span> Takami Bridal Attire
+                </li>
+                <li>
+                  <span className="icon_check"></span> Esthetic Salon
+                </li>
+                <li>
+                  <span className="icon_check"></span> Multilingual staff
+                </li>
+                <li>
+                  <span className="icon_check"></span> Dry cleaning and laundry
+                </li>
+                <li>
+                  <span className="icon_check"></span> Credit cards accepted
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-6">
+              <ul>
+                <li>
+                  <span className="icon_check"></span> Rent-a-car
+                </li>
+                <li>
+                  <span className="icon_check"></span> Reservation &
+                  confirmation
+                </li>
+                <li>
+                  <span className="icon_check"></span> Babysitter upon request
+                </li>
+                <li>
+                  <span className="icon_check"></span> 24-hour currency exchange
+                </li>
+                <li>
+                  <span className="icon_check"></span> 24-hour Manager on Duty
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="embla" ref={emblaRef}>
+            <div className="embla__container">
+              {field?.image.map((img, j) => {
+                return (
+                  <div
+                    key={j}
+                    className="embla__slide embla__slide__padded"
+                    style={{
+                      backgroundImage: `url(${img.url})`,
+                      backgroundSize: "cover",
+                    }}
+                  ></div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
 
-			{/* <!-- Field Details Section Begin --> */}
-			<section className="room-details spad">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-12">
-							<div className="room__details__content">
-								<div className="room__details__title">
-									<h2>{field.name}</h2>
-									<Link to={`/booking/${field.id}`} className="primary-btn">
-										Book Now
-									</Link>
-								</div>
-								<div className="room__details__desc">
-									<h2>Description:</h2>
-									<p>
-										We’re halfway through the summer, but while plenty of people
-										are kicking back and enjoying their vacations, the social
-										media development teams likely aren’t doing the same. In the
-										past two weeks alone, we’ve seen four big new updates that
-										can directly impact the social marketing campaigns of
-										hotels, resorts, and other businesses in the hospitality
-										industry. Let’s take a close look at each one.
-									</p>
-									<p>
-										The new desktop version of the site is significantly
-										improved, which will make it easier for hotels and resorts
-										to navigate the platform.
-									</p>
-									<p>
-										There is one big change though that we want to note, and
-										that’s the more live video and local moments (the latter of
-										which are based on your location). These will be prioritized
-										in users’ feeds, so take advantage of this and create this
-										content to improve your reach and connect with more members
-										of your target audience.
-									</p>
-									<p>
-										We’ve gotten yet another new feature for Instagram Stories,
-										and this time it’s the Chat sticker, which allows you to
-										invite Story followers to join in on a new group chat.
-										Instagram is currently advertising this as a way to
-										jumpstart big group conversations or make plans.
-									</p>
-								</div>
-								<div className="row">
-									<div className="col-lg-6 col-md-6 col-sm-6">
-										<div className="room__details__facilities">
-											<h2>Others facilities:</h2>
-											<div className="row">
-												<div className="col-lg-6">
-													<ul>
-														<li>
-															<span className="icon_check"></span> Takami Bridal
-															Attire
-														</li>
-														<li>
-															<span className="icon_check"></span> Esthetic
-															Salon
-														</li>
-														<li>
-															<span className="icon_check"></span> Multilingual
-															staff
-														</li>
-														<li>
-															<span className="icon_check"></span> Dry cleaning
-															and laundry
-														</li>
-														<li>
-															<span className="icon_check"></span> Credit cards
-															accepted
-														</li>
-													</ul>
-												</div>
-												<div className="col-lg-6">
-													<ul>
-														<li>
-															<span className="icon_check"></span> Rent-a-car
-														</li>
-														<li>
-															<span className="icon_check"></span> Reservation &
-															confirmation
-														</li>
-														<li>
-															<span className="icon_check"></span> Babysitter
-															upon request
-														</li>
-														<li>
-															<span className="icon_check"></span> 24-hour
-															currency exchange
-														</li>
-														<li>
-															<span className="icon_check"></span> 24-hour
-															Manager on Duty
-														</li>
-													</ul>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div className="col-lg-6 col-md-6 col-sm-6">
-										<div className="room__details__more__facilities">
-											<h2>Most popular facilities:</h2>
-											{equipments.map((equipment, i) => (
-												<>
-													<div className="row">
-														<div className="col-lg-6">
-															<div className="room__details__more__facilities__item">
-																<div className="icon">
-																	<img
-																		src="img/rooms/details/facilities/fac-1.png"
-																		alt=""
-																	/>
-																</div>
-																<h6>{equipment.name}</h6>
-															</div>
-														</div>
-													</div>
-												</>
-											))}
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			{/*  <!-- Field Details Section End --> */}
-			<Footer />
-		</>
-	);
+      {/* <!-- Field Details Section Begin --> */}
+      <section className="room-details spad">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="room__details__content">
+                <div className="room__details__title">
+                  <h2>{field.name}</h2>
+                  <Link to={`/booking/${field.id}`} className="primary-btn">
+                    Book Now
+                  </Link>
+                </div>
+                <div className="room__details__desc">
+                  <h2>Description:</h2>
+                  <p>
+                    {field.name}, is a {field.Category.name} stadium.</p><p>  The stadium in{field.adress}. With a length {field.length}m, and width
+                     {field.width}m</p><p>It is the large stadium. The hour price of
+                    the field {field.hourPrice}$, where the working hour of the
+                    field from {field.from} to {field.to}.</p>
+					<p>We hope for you enjoyable match
+                  </p>
+                </div>
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-sm-6">
+                    <div className="room__details__facilities">
+                      <h2>Others facilities:</h2>
+                      <div className="row">
+                        <div className="col-lg-6">
+                          <ul>
+                            <li>
+                              <span className="icon_check"></span> Takami Bridal
+                              Attire
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> Esthetic
+                              Salon
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> Multilingual
+                              staff
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> Dry cleaning
+                              and laundry
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> Credit cards
+                              accepted
+                            </li>
+                          </ul>
+                        </div>
+                        <div className="col-lg-6">
+                          <ul>
+                            <li>
+                              <span className="icon_check"></span> Rent-a-car
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> Reservation &
+                              confirmation
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> Babysitter
+                              upon request
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> 24-hour
+                              currency exchange
+                            </li>
+                            <li>
+                              <span className="icon_check"></span> 24-hour
+                              Manager on Duty
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-sm-6">
+                    <div className="room__details__more__facilities">
+                      <h2>Most popular facilities:</h2>
+                      {equipments.map((equipment, i) => (
+                        <>
+                          <div className="row">
+                            <div className="col-lg-6">
+                              <div className="room__details__more__facilities__item">
+                                <div className="icon">
+                                  <img
+                                    src="img/rooms/details/facilities/fac-1.png"
+                                    alt=""
+                                  />
+                                </div>
+                                <h6>{equipment.name}</h6>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/*  <!-- Field Details Section End --> */}
+      <Footer />
+    </>
+  );
 };
 
 export default FieldDetails;
