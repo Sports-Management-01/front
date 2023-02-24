b6ee5456ead5164fd39a04bae2d537e0a02736d6
Meis01 <meis.baas95@hotmail.com>
2023-02-24 16:28:32 +0300

    signup was updated

--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -2,7 +2,9 @@ import './SignUp.css';
 import { useState } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 import Header from '../../components/Header/Header';
-import logo2 from '../../assets/logo/logo2.PNG'
+import toptepe from '../../assets/img/toptepe.png';
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
 
 const SignUp = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -51,12 +53,81 @@ const SignUp = () => {
 
         <>
             <Header />
-          <div classNameName="container my-4">
+            <div className="container">
+                <div className="d-flex justify-content-center h-100 p-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>Sign Up</h3>
+
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit}>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><PersonIcon /></span>
+                                    </div>
+                                    <input type="text" name="name" className="form-control" placeholder="Username" onChange={handleOnChange} />
+                                </div>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><PersonIcon /></span>
+                                    </div>
+                                    <input type="email" name="email" className="form-control" placeholder="Email" onChange={handleOnChange} />
+                                </div>
+                                <div className=" mb-3 input-group form-group">
+                                    <label htmlFor="accountType" className="form-label mb-3 m-2">User</label>
+                                    <input type="radio"  name='roleId' value="3"  onChange={handleOnChange} />
+                                    <label htmlFor="accountType" className="form-label mb-3 m-2">Company</label>
+                                    <input type="radio" name='roleId' value="2"  onChange={handleOnChange} />
+                                </div>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><LockIcon /></span>
+                                    </div>
+                                    <input type="phone" name="phone" className="form-control" placeholder="Phone" onChange={handleOnChange} />
+                                </div>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><LockIcon /></span>
+                                    </div>
+                                    <input type="password" name="password" className="form-control" placeholder="Password" onChange={handleOnChange} />
+                                </div>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><LockIcon /></span>
+                                    </div>
+                                    <input type="password" name="passwordConfirmation" className="form-control" placeholder="PasswordConfirmation" onChange={handleOnChange} />
+                                </div>
+                                <div className="input-group form-group">
+                                <div className="form-group">
+                                    <input type="submit" value="SignUp" className="btn float-right login_btn" />
+                                </div>
+                                       {/*  <button type="submit" className="btn btn-primary1 w-50" disabled={isLoading}>
+                                            {isLoading ? "Please Wait" : "Register"}
+                                        </button> */}
+                                                                  
+                                 </div>
+                            </form>
+                        </div>
+                        <div className="card-footer">
+                            <div className="d-flex justify-content-center links">
+                                If you have an account?<a href="/signin">Sign In</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            {/* ENDDDDDDDDDDDDDD */}
+         {/*  <div classNameName="container my-4">
                 <div classNameName='row'>
                     <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
                         <div className="Register_register__csfwh my-5 p-5 " >
                             <div className='Register_logo__-jKtP mb-4'>
-                                <img className='logo-img' src={logo2} alt="react-logo"/>
+                                <img className='logo-img' src={toptepe} alt="react-logo"/>
 
                             </div>
 
@@ -105,79 +176,8 @@ const SignUp = () => {
                         </div>
                     </div>
                 </div>
-            </div> 
-{/* <section className="vh-100" style={{backgroundColor: "#eee"}}>
-  <div className="container h-100">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-lg-12 col-xl-11">
-        <div className="card text-black" style={{borderRadius: 25}}>
-          <div className="card-body p-md-5">
-            <div className="row justify-content-center">
-              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+            </div>  */}
 
-                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
-                <form className="mx-1 mx-md-4">
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" className="form-control" />
-                      <label className="form-label" for="form3Example1c">Your Name</label>
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" className="form-control" />
-                      <label className="form-label" for="form3Example3c">Your Email</label>
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" className="form-control" />
-                      <label className="form-label" for="form3Example4c">Password</label>
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4cd" className="form-control" />
-                      <label className="form-label" for="form3Example4cd">Repeat your password</label>
-                    </div>
-                  </div>
-
-                  <div className="form-check d-flex justify-content-center mb-5">
-                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                    <label className="form-check-label" for="form2Example3">
-                      I agree all statements in <a href="#!">Terms of service</a>
-                    </label>
-                  </div>
-
-                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" className="btn btn-primary btn-lg">Register</button>
-                  </div>
-
-                </form>
-
-              </div>
-              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                  className="img-fluid" alt="Sample image"/>
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section> */}
 
         </>
 
